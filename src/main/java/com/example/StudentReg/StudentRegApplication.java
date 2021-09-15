659be8c9f57aeebd55bd98fdef32e1c0cb3405c2
Vihangi98 <vihangisenadheera@gmail.com>
2021-09-15 20:11:53 +0530

    StudentReg

--- src/main/java/com/example/StudentReg/StudentRegApplication.java
+++ src/main/java/com/example/StudentReg/StudentRegApplication.java
@@ -2,7 +2,7 @@ package com.example.StudentReg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//comment
+//comments
 @SpringBootApplication
 public class StudentRegApplication {
 
